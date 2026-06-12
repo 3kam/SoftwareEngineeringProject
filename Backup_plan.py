3dcc4c0648c0c123d887d0733e684aed4b3997c5
Ekam <ekames24@gmail.com>
2026-06-12 00:24:21 +0000

    fixed full_destiantion_path

--- Backup_plan.py
+++ Backup_plan.py
@@ -17,7 +17,7 @@ def execute_production_database_backup():
 
     # Copy the database file cleanly while preserving system metadata
     shutil.copy2(primary_database_file, full_destination_path)
-    print(f"[BACKUP SUCCESSFULL] File created at: {full_destination_pat}")
+    print(f"[BACKUP SUCCESSFULL] File created at: {full_destination_path}")
 
 if __name__ == "__main__":
-    execute_production_database_backup()
+    execute_production_database_backup
